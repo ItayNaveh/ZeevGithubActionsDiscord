e411941c00e002e83509a8eb2408c973b0f62373
Itay <naveh.itay@gmail.com>
2020-12-12 12:45:12 +0000

    comment alot

--- .github/actions/discord-notifier/src/index.js
+++ .github/actions/discord-notifier/src/index.js
@@ -1,47 +1,20 @@
 const core = require("@actions/core");
 // const github = require("@actions/github");
-const fetch = require("node-fetch").default;
+// const fetch = require("node-fetch").default;
 
 const main = async() => {
     try {
         const webhookID = core.getInput("webhook-id");
-        // const webhookToken = core.getInput("webhook-token");
+        const webhookToken = core.getInput("webhook-token");
         const status = core.getInput("status");
         //success
         //failure
 
-        // console.log("status::");
-        // console.log(await (await fetch(`https://post-test.herokuapp.com/single/${status}`)).json());
 
-        // console.log("webhook id::");
-        // console.log(await (await fetch(`https://post-test.herokuapp.com/single/${webhookID}`)).json());
-
-        // console.log("webhook token::");
-        // console.log(await (await fetch(`https://post-test.herokuapp.com/single/${webhookToken}`)).json());
-
-        // console.log("id and token::");
-        // console.log(await (await fetch(`https://post-test.herokuapp.com/double/${webhookID}/${webhookToken}`)).json());
-
-        // console.log("id and token::");
-        // console.log(await (await fetch(`https://post-test.herokuapp.com/body`, {method: "POST", headers: {"Content-Type": "application/json"}, body: {status: status, id: webhookID, token: webhookToken}})).json());
-
-        console.log("id", webhookID);
-        const response = await fetch("https://discord.com/api/webhooks/779046008684412979/UaQqJmOHg7nwqNAupSc04_hiE-Hmrk61VwTwtgMEnuLcXFe95S29omkXHE-ikOzNk5bZ", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: {
-                embeds: [
-                    {
-                        title: "Title",
-                        description: `${status}`
-                    }
-                ]
-            }
-        });
-        const data = await response.json();
-        console.log("status -", response.status);
-        console.log(data);
-        console.log("done");
+        // const data = await response.json();
+        // console.log("status -", response.status);
+        // console.log(data);
+        // console.log("done");
         // console.log("action:", github.context.action);
         // console.log("actor: ", github.context.actor);
         // console.log("eventName:", github.context.eventName);
